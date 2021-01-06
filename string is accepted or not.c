c0696f3e0f0e97ff2a83bcd4ce7291c5f26e398e
shivanichaudhary12 <69896496+shivanichaudhary12@users.noreply.github.com>
2021-01-06 15:47:27 +0530

    Create string is accepted or not.c

---
+++ string is accepted or not.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+using namespace std;
+
+char a[100];
+int n,i;
+
+int main()
+{
+    cout<<"\nenter string:";
+    cin>>a;
+    
+    if(a[0]=='a'&&(a[n-1]=='a'||a[n-1]=='b')&&a[n-2]=='c')
+    {
+        for (i=1;i<n-2;i++)
+        {
+            if(a[i]!='b')
+            {
+                cout<<"string is not accepted";
+                exit(0);
+            
+            }
+            
+        }
+        cout<<"string is accepted";
+        }
+        else
+        cout<<"string is not accepted";
+}
+
